4e32e4ade8544469fdc77b8ace3fb6915a9fe803
Fargier Sylvain <sfargier@wyplay.com>
2009-11-10 14:05:36 +0100

    updating test procedure, now it can be distchecked

--- tests/test_gcc.sh
+++ tests/test_gcc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 CURR_PATH=$(cd $(dirname $0) ; pwd)
+[ -n "$abs_top_builddir" ] || abs_top_builddir=$CURR_PATH/..
 
 . ${CURR_PATH}/utils.sh
 
@@ -10,7 +11,7 @@ echo "
 x86_64-pc-linux-gnu-gcc -O2 -I/usr/include test.c -o test.o -march=nocona
 gcc test.c -o test
 
-" | ${CURR_PATH}/../src/colorit > $TMPDIR
+" | ${abs_top_builddir}/src/colorit > $TMPDIR
 
 awk '/\033\[1\;34mx86_64-pc-linux-gnu-gcc\033\[0m/ { exit 1 }' $TMPDIR && die "FAILED: gcc not highlighted"
 awk '/\033\[1\;34m-O2\033\[0m/ { exit 1 }' $TMPDIR && die "FAILED: optim not highlighted"
